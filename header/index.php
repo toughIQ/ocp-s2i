044d1c88fb1e4a09f8228f44ad5f86aba2d52bbb
Chris Tawfik <toughiq@redhat.com>
2022-10-31 13:29:23 +0100

    Update index.php

--- header/index.php
+++ header/index.php
@@ -5,7 +5,7 @@
   //header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
   //or, if you DO want a file to cache, use:
-  header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
+  header("Cache-Control: public"); //30days (60sec * 60min * 24hours * 30days)
 
 ?>
 
